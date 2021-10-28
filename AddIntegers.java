c46bb4c4dda2c11ccd330c69267efd719d9ba4d2
Maria <mariaalb@umich.edu>
2021-10-28 17:09:29 -0400

    add new 2 integers

--- AddIntegers.java
+++ AddIntegers.java
@@ -2,7 +2,7 @@ public class AddIntegers {
 
     public static void main(String[] args) {
 
-        int first = 50;
+        int first = 40;
         int second = 50;
 
         System.out.println("Enter two numbers: " + first + " " + second);
